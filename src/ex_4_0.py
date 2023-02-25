2d2d3348b5a7d82734825ddbb9397cd79158c5cd
Sudheer2580 <123262735+Sudheer2580@users.noreply.github.com>
2023-02-25 01:42:59 -0500

    update

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -1,10 +1,21 @@
+""" ex_4_0.py """
+try:
+    from src.util import get_data_file_path
+except ImportError:
+    from util import get_data_file_path
+
+# Use this FILENAME variable to test your function.
+FILENAME = get_data_file_path('messages.log')
+# >>>> DO NOT MODIFY CODE ABOVE <<<<
 """ sudheer
 Module 4
 """
 
 
 def get_shutdown_events(logfile):
-    """ Function 1"""
+    """
+    Your docstring here.  Replace the pass keyword below with your implementation
+    """
     f = open(logfile, 'rt')
     x = list()
     for i in f:
@@ -14,5 +25,8 @@ def get_shutdown_events(logfile):
     for i in x:
         if i.split()[4] == 'initiated.':
             final_lis.append(i[:-2])
-
     return final_lis
+
+# >>>> The code below will call your function and print the results
+if __name__ == "__main__":
+    print(f"{get_shutdown_events(FILENAME)=}")
